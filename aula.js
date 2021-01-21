d8dcb5f1aa9ce2ca796e31837a360fb1b3684c45
icsalgado <icsalgado@icloud.com>
2021-01-21 01:02:44 -0300

    default functions arguments

--- aula.js
+++ aula.js
@@ -5,3 +5,9 @@ var  sum = (a, b) => {
 
 console.log(sum(5, 15));
 
+//default functions arguments
+function multiply(a, b = 1){//aqui eu posso atribuir um valor a uma variavel não informada nos parametros
+    return a * b;
+}
+
+console.log(multiply(5))
