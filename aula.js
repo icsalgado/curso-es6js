d5c80e3921c04bd32a268a7400024c964136dc80
icsalgado <icsalgado@icloud.com>
2021-01-21 21:54:47 -0300

    Tornando objeto iteravel com sumbol, possibilitando usar spread, etc

--- aula.js
+++ aula.js
@@ -1,13 +1,36 @@
 //symbols
 //maneira de gerar um identificador unico
 //não se invoca usando new, o valor passado dentro do symbol serve para debug
-//well known symbols
+//well known symbols - adiciona capacidades, inclusive em objetos
+//interface para fazer operações
+
 
 const uniqueId = Symbol('hello');
 
-const arr = [1, 2, 3, 4];
+Symbol.iterator;
 
-const it = arr[Symbol.iterator]();
+const arr = [1, 2, 3, 4];
+const str = 'DIO';
+
+arr[Symbol.iterator]().next();
+
+const obj = {
+    values: [1, 2, 3, 4],
+    [Symbol.iterator](){
+        let i = 0;
+        return {
+            next: () => {
+                i++;
+                return {
+                    value: this.values[i - 1],
+                    done: i > this.values.length 
+                };
+            }
+        };
+    }
+};
+
+const it = obj[Symbol.iterator]();
 
 console.log(it.next());
 console.log(it.next());
