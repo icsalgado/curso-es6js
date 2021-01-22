bb9be6d23c5cef3a8f130e811f87a7692ce54835
icsalgado <icsalgado@icloud.com>
2021-01-21 21:37:18 -0300

    Symbols

--- aula.js
+++ aula.js
@@ -1,37 +1,12 @@
-//Rest operator ... dentro dos argumentos - pega todos os parametros e transforma num array
+//symbols
+//maneira de gerar um identificador unico
+//não se invoca usando new, o valor passado dentro do symbol serve para debug
+//well known symbols
 
-function sum(...args) {
-    return args.reduce((acumulado, value) => acumulado + value, 0);//evita o uso de linhas que implementa a soma de todo o array
-}
+const uniqueId = Symbol('hello');
 
-console.log(sum(5, 5, 5, 2, 3));
-
-//spread operator - pega array e transforma em argumentos para a função
-
-const str = 'DIO';
-const arr = [1, 2, 3, 4]
-const arr2 = [5, 6, 7]
-
-function logArgs(...args){
-    console.log(args);
-}
-
-logArgs(...str);//aplica o spread
-logArgs(...arr);
-const arrConcatenado = [...arr, ...arr2]; //substitui o concat
-console.log(arrConcatenado);
-
-const obj = {//so se pode criar spread em objetos gerando novos objetos
-    test: 123
-};
-const obj2 = {
-    ...obj,
-    test2: 'hello'
+const obj = {
+    [uniqueId]: 'hello'
 };
 
-console.log(obj2);
-
-//Destructuring Assignment
-
-var [maca2, banan2, laranja2, [tomate2]] = ['maca', 'banana', 'laranja', ['tomate']];
-//evita a criação de uma variavel para destruir o array para cada posição
+console.log(obj);
